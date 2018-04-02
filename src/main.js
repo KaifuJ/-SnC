37eb9634bedfb9e4c6747092ca925106fa634845
unknown <jkf0128@gmail.com>
2018-04-01 23:08:00 -0700

    change directory name

--- src/main.js
+++ src/main.js
@@ -11,12 +11,34 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: ['pages/snc/main', '^pages/todo/main'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      list: [
+        {
+          pagePath: 'pages/todo/main',
+          text: 'Todo',
+          iconPath: 'images/todo.png',
+          selectedIconPath: 'images/todo_s.png'
+        },
+        {
+          pagePath: 'pages/snc/main',
+          text: 'SnC',
+          iconPath: 'images/snc.png',
+          selectedIconPath: 'images/snc_s.png'
+        },
+        {
+          pagePath: 'pages/rank/main',
+          text: 'Rank',
+          iconPath: 'images/rank.png',
+          selectedIconPath: 'images/rank_s.png'
+        }
+      ]
     }
   }
 }
